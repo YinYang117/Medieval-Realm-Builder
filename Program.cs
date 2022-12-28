8f601c72cc6b7a08d793a9f61b5b8e8ec6e05cf4
yinyang117 <slcryan2010@yahoo.com>
2022-12-27 23:49:34 -0700

    c# start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Medieval_Realm_Builder;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Medieval Realm Builder!");
+        Console.WriteLine("Who is playing?");
+        var name = Console.ReadLine();
+        var currDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currDate:d} at {currDate:t}!");
+        Console.Write($"\nPress any key to exit...");
+        Console.ReadKey(true);
+    }
+}
---
+++ commands.md
@@ -0,0 +1,7 @@
+<!-- Setup: -->
+dotnet new console --framework net6.0 --use-program-main
+<!-- prompt created .vscode, bin, Debug, net6.0, obj for me automatically. -->
+
+<!-- using our .NET CaLI we run the program with: -->
+dotnet run
+
